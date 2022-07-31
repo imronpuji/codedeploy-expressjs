c2c6341e643c234e057eb02e6c494b468f455d3b
muhamad imron <imronpuji5@gmail.com>
2022-07-31 17:17:32 +0700

    change pattern

--- codedeploy/app_start.sh
+++ codedeploy/app_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /usr/cddemo
-pm2 start app.js > app.out.log 2> app.err.log < /dev/null & 
+sudo pm2 start app.js > app.out.log 2> app.err.log < /dev/null & 
--- codedeploy/app_stop.sh
+++ codedeploy/app_stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /usr/cddemo
-pm2 stop app.js > app.out.log 2> app.err.log < /dev/null & 
+sudo pm2 stop app.js > app.out.log 2> app.err.log < /dev/null & 
