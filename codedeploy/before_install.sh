5d4778a93f12398781489b6b9db99617be340aeb
muhamad imron <imronpuji5@gmail.com>
2022-07-31 15:00:36 +0700

    reinstall node js on beforeInstall hook

--- codedeploy/before_install.sh
+++ codedeploy/before_install.sh
@@ -1,3 +1,10 @@
+yum -y update
+
+curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
+yum -y install nodejs
+
+npm install --location=global pm2
+pm2 update
 
 export app_root=/usr/cddemo
 if [ -d "$app_root" ];then
